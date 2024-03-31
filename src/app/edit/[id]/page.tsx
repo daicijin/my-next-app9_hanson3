cd686dc623e70206ce4b5ec28a133e77042fa867
daicijin <hayashid0527@gmail.com>
2024-03-31 17:41:26 +0900

    second commit

--- src/app/edit/[id]/page.tsx
+++ src/app/edit/[id]/page.tsx
@@ -1,20 +1,23 @@
-import BookDetails from '@/components/BookDetails';
-import FormEdit from '@/components/FormEdit';
-import { getBookById, getReviewById } from '@/lib/getter';
+import BookDetails from "@/components/BookDetails"
+import FormEdit from "@/components/FormEdit"
+import { getBookById, getReviewById } from "@/lib/getter"
 
-export default async function EditPage({ params }) {
-  const book = await getBookById(params.id);
-  const review = await getReviewById(params.id);
-  const read = (review?.read || new Date()).toLocaleDateString('sv-SE');
-  // const read = (review?.read || new Date()).toLocaleDateString('ja-JP',
-  //   { year: 'numeric', month: '2-digit', day: '2-digit' }
-  // ).replaceAll('/', '-')
+type Props = {
+  params: {
+    id: string
+  }
+}
+
+export default async function EditPage({ params }: Props) {
+  const book = await getBookById(params.id)
+  const review = await getReviewById(params.id)
+  const read = (review?.read || new Date()).toLocaleDateString("sv-SE")
 
   return (
     <div id="form">
       <BookDetails book={book} />
       <hr />
-      <FormEdit src={{ id: book.id, read, memo: review?.memo }} />
+      <FormEdit src={{ id: book.id, read, memo: review?.memo as string }} />
     </div>
-  );
+  )
 }
